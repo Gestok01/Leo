dbc28eabeb820321351dde4f6b272eade08b01ee
Gestok01 <uday2210007@akgec.ac.in>
2025-08-01 21:35:22 +0530

    Fixed signup/signin to use full backend URL

--- frontend/src/landing_page/signup/Signin.js
+++ frontend/src/landing_page/signup/Signin.js
@@ -9,7 +9,8 @@ function Signin() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await fetch(`/api/auth/signin`, {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/signin`, {
+
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -11,7 +11,8 @@ function Signup() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await fetch(`/api/auth/signup`, {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/signup`, {
+
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ fullName, email, username, password })
